432688f21b1a58874b891fed525cb036bc32d3ff
Tomer Levi <levitomer579@gmail.com>
2024-12-01 12:56:23 +0200

    Added map to the bottom of the page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import { Button, TextField, Dropdown } from "@vibe/core";
 import "@vibe/core/tokens"; // Load CSS tokens
 
+import { BasicMap } from "@levit0mer/smartmaps";
+
 function App() {
   const [loading, setLoading] = useState(true);
 
@@ -24,6 +26,12 @@ function App() {
       <div style={{margin: "auto" }}>
         <div style={{ padding: "20px", maxWidth: "500px", margin: "auto" }}>
           <h1>Your Simple Task Manager</h1>
+        </div>
+        <div style={{ padding: "0", maxWidth: "100%", height: "400px", margin: "auto" }}>
+          <BasicMap
+            center={[34.7818, 32.0853]} // Longitude, Latitude of Tel Aviv
+            zoom={12} // Adjust zoom for a closer view
+          />
         </div>
       </div>
     </>
